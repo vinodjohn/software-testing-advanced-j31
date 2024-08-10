f7df1d67a7f5a09a2c516866601f3fc22a4bf5c9
Vinod John <vinodjohn56@gmail.com>
2024-08-10 11:06:57 +0300

    Initial Commit

---
+++ src/main/java/org/sda/j31/softwaretestingadvanced/SoftwareTestingAdvancedApplication.java
@@ -0,0 +1,13 @@
+package org.sda.j31.softwaretestingadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTestingAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareTestingAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sda/j31/softwaretestingadvanced/SoftwareTestingAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sda.j31.softwaretestingadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestingAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
